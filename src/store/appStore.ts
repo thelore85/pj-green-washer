f4fc865df47679340fc300e4584a92913ad5255e
TheLore85 <garofalo.digital@gmail.com>
2025-06-25 18:07:38 +0200

    zustand: store mgmt setup

--- src/store/appStore.ts
+++ src/store/appStore.ts
@@ -1,28 +1,30 @@
 import { create } from 'zustand'
 import type { AppStore } from './appStoreType'
-import { persist, createJSONStorage } from 'zustand/middleware'
+import { persist, createJSONStorage, devtools } from 'zustand/middleware'
 
 export const useStore = create<AppStore>()(
-  persist(
-    (set) => ({
-      // Data Store
-      init: false,
-      cardSelected: null,
-      articleSelected: null,
-      claimCards: [],
-      articleList: [],
-      searchQuery: '',
+  devtools(
+    persist(
+      (set) => ({
+        // Data Store
+        init: false,
+        cardSelected: null,
+        articleSelected: null,
+        claimCards: [],
+        articleList: [],
+        searchQuery: '',
 
-      // Actions
-      setSearchQuery: (query) => set({ searchQuery: query }),
-      initDataApp: (data) => set({ articleList: data, init: true }),
-      restartApp: () => set({ init: false, cardSelected: null, articleSelected: null, claimCards: [], articleList: [], searchQuery: '' }),
-      selectCard: (id) => set({ cardSelected: id }),
-      selectArticle: (id) => set({ articleSelected: id }),
-    }),
-    {
-      name: 'appStorage',
-      storage: createJSONStorage(() => sessionStorage),
-    },
+        // Actions
+        setSearchQuery: (query) => set({ searchQuery: query }),
+        initDataApp: (data) => set({ articleList: data, init: true }),
+        restartApp: () => set({ init: false, cardSelected: null, articleSelected: null, claimCards: [], articleList: [], searchQuery: '' }),
+        selectCard: (id) => set({ cardSelected: id }),
+        selectArticle: (id) => set({ articleSelected: id }),
+      }),
+      {
+        name: 'appStorage',
+        storage: createJSONStorage(() => sessionStorage),
+      },
+    ),
   ),
 )
