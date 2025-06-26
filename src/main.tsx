b4800cd1b9b5add4220f27ca33b089c900d0364f
TheLore85 <garofalo.digital@gmail.com>
2025-06-26 11:03:43 +0200

    removing react router

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '@/styles/index.css'
 import App from '@/App'
-import { BrowserRouter } from 'react-router'
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <StrictMode>
-      <App />
-    </StrictMode>
-  </BrowserRouter>,
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
