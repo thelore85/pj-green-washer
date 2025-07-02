70b03cef254c8b493e510956085b483b5d6663e8
Yoel Cabaleiro <yoelcabaleiro@gmail.com>
2025-07-02 15:52:18 +0100

    version 1.3

--- src/components/navigation/FooterApp.tsx
+++ src/components/navigation/FooterApp.tsx
@@ -1,3 +1,3 @@
 export default function FooterApp() {
-  return <footer className="bg-card flex h-[4dvh] items-center justify-center text-center text-xs text-gray-400">This is the footer disclaimer</footer>
+  return <footer className="bg-card flex h-[4dvh] items-center justify-center text-center text-xs text-gray-400">Maska | 2025 | Disclaimer: Esto es una prueba de concepto de una aplicación que se encuentra actualmente en desarrollo. Los resultados mostrados son provisionales.</footer>
 }
